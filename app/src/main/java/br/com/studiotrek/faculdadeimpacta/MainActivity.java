2eac1e98c37cf89fdbe7532110138a7d9788669e
kleber99 <kleber_nunes99@hotmail.com>
2018-02-14 16:00:59 -0200

    inicial

---
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/MainActivity.java
@@ -0,0 +1,27 @@
+package br.com.studiotrek.faculdadeimpacta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView wvMainPrincipal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wvMainPrincipal = findViewById(R.id.wv_main_principal);
+
+        WebSettings webSettings = wvMainPrincipal.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        wvMainPrincipal.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wvMainPrincipal.setWebViewClient(new WebViewClient());
+        wvMainPrincipal.loadUrl("https://www.impacta.edu.br/");
+    }
+}
