b94c118a6d0a0d60348f23f0019a8ff55c9b0eeb
Wilder Pereira <wilder_roberto@hotmail.com>
2018-02-18 23:32:03 -0300

    add api calls to retrofit interface

--- app/src/main/java/br/com/studiotrek/faculdadeimpacta/domain/repository/ImpactaApi.kt
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/domain/repository/ImpactaApi.kt
@@ -1,5 +1,29 @@
 package br.com.studiotrek.faculdadeimpacta.domain.repository
 
+import org.json.JSONObject
+import retrofit2.http.*
+import rx.Observable
+
 /**
  * Created by Wilder on 18/02/18.
  */
+
+interface UrlShortenerApi {
+
+    //TODO: create response class
+    @GET("login/{ra}/{password}")
+    fun login(@Path("ra") ra: String, @Path("password") password: String): Observable<JSONObject>
+
+    //TODO: Change jsonobject to appropriate classes
+    @POST("horario")
+    fun getClassesSchedule(@Body body: JSONObject): Observable<JSONObject>
+
+    //TODO: Change jsonobject to appropriate classes
+    @POST("quadro-horario")
+    fun getStudentInformation(@Body body: String): Observable<JSONObject>
+
+    //TODO: Change jsonobject to appropriate classes
+    @POST("semestre-nota")
+    fun getCurrentSemesterGrades(@Body body: String): Observable<JSONObject>
+
+}
