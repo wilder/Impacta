8f424d20a3c3433b512dfa6e0d7643fdae36c1a8
Wilder Pereira <wilder_roberto@hotmail.com>
2018-02-19 00:20:28 -0300

    fix dagger unresoulved reference component error

---
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/login/LoginPresenter.kt
@@ -0,0 +1,5 @@
+package br.com.studiotrek.faculdadeimpacta.presentation.login
+
+/**
+ * Created by Wilder on 18/02/18.
+ */
---
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/login/LoginResponse.kt
@@ -0,0 +1,9 @@
+package br.com.studiotrek.faculdadeimpacta.presentation.login
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Wilder on 18/02/18.
+ */
+class LoginBody(@SerializedName("cookin") @Expose var cookie: String)
