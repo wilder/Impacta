70be94b03a3b6f8de560efd96b0e610dda8d8717
Wilder Pereira <wilder_roberto@hotmail.com>
2018-02-18 22:54:24 -0300

    convert existing java files to kotlin

---
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/MainActivity.kt
@@ -0,0 +1,26 @@
+package br.com.studiotrek.faculdadeimpacta
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+@SuppressLint("SetJavaScriptEnabled")
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val wvMainPrincipal = findViewById<WebView>(R.id.wv_main_principal)
+
+        val webSettings = wvMainPrincipal.settings
+        webSettings.javaScriptEnabled = true
+
+        wvMainPrincipal.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
+        wvMainPrincipal.webViewClient = WebViewClient()
+        wvMainPrincipal.loadUrl("http://account.impacta.edu.br/index.php")
+    }
+}
