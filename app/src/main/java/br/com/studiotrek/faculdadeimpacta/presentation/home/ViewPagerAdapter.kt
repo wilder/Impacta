e7e0708cbc521418a1219ec8169badeba1550e15
Wilder Pereira <wilder_roberto@hotmail.com>
2018-03-05 00:03:25 -0300

    add ViewPager to MenuActivity

---
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/home/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package br.com.studiotrek.faculdadeimpacta.presentation.home
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+import java.util.ArrayList
+
+/**
+ * Created by Wilder on 04/03/18.
+ */
+
+class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+    private val mFragmentList = ArrayList<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment) {
+        mFragmentList.add(fragment)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return ""
+    }
+}
--- app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/menu/MenuActivity.kt
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/menu/MenuActivity.kt
@@ -1,11 +1,17 @@
 package br.com.studiotrek.faculdadeimpacta.presentation.menu
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import br.com.studiotrek.faculdadeimpacta.App
 import br.com.studiotrek.faculdadeimpacta.R
+import br.com.studiotrek.faculdadeimpacta.presentation.home.HomeFragment
+import br.com.studiotrek.faculdadeimpacta.presentation.home.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_menu.*
 import javax.inject.Inject
 
+@SuppressLint("Registered")
 /**
  * Created by kleber on 02/03/2018.
  */
@@ -14,6 +20,7 @@ class MenuActivity : AppCompatActivity(), MenuPresenter.View {
     @Inject
     lateinit var presenter: MenuPresenter
     private val TAG: String = "MenuActivity"
+    lateinit var viewPagerAdapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +31,14 @@ class MenuActivity : AppCompatActivity(), MenuPresenter.View {
     private fun init() {
         (application as App).component.inject(this)
         presenter.bindView(this)
+        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+        setupViewPager(viewpager)
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment.newInstance())
+        viewPager.adapter = adapter
     }
 
 }
