ff46911aa021866024e494ba1bbbc473eca40847
kleber99 <kleber_nunes99@hotmail.com>
2018-03-11 11:41:25 -0300

    adicionando splash

--- app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/splash/SplashActivity.kt
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/splash/SplashActivity.kt
@@ -3,8 +3,9 @@ package br.com.studiotrek.faculdadeimpacta.presentation.splash
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
-import br.com.studiotrek.faculdadeimpacta.domain.entity.Student
+import br.com.studiotrek.faculdadeimpacta.presentation.login.LoginActivity
 import br.com.studiotrek.faculdadeimpacta.presentation.menu.MenuActivity
+import br.com.studiotrek.faculdadeimpacta.utils.PreferencesManager
 
 
 /**
@@ -15,8 +16,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
+        if(PreferencesManager(this).isUserSet()) {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        } else {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        
         finish()
     }
 
