a95502baa2a49bde1ca3f5ea238b18e87d5619ae
Wilder Pereira <wilder_roberto@hotmail.com>
2018-03-11 23:17:15 -0300

    log main events to analytics

--- app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/login/LoginActivity.kt
+++ app/src/main/java/br/com/studiotrek/faculdadeimpacta/presentation/login/LoginActivity.kt
@@ -11,8 +11,9 @@ import br.com.studiotrek.faculdadeimpacta.R
 import br.com.studiotrek.faculdadeimpacta.domain.entity.CookieDTO
 import br.com.studiotrek.faculdadeimpacta.domain.entity.Student
 import br.com.studiotrek.faculdadeimpacta.presentation.menu.MenuActivity
+import br.com.studiotrek.faculdadeimpacta.utils.Analytics
 import br.com.studiotrek.faculdadeimpacta.utils.PreferencesManager
-import com.jakewharton.rxbinding.view.visibility
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_login.*
 import javax.inject.Inject
 
@@ -21,10 +22,13 @@ class LoginActivity : AppCompatActivity(), LoginPresenter.View {
     @Inject
     lateinit var presenter: LoginPresenter
     private val TAG: String = "LoginActivity"
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         init()
     }
 
@@ -59,9 +63,10 @@ class LoginActivity : AppCompatActivity(), LoginPresenter.View {
     }
 
     override fun successfulLogin(cookie: LoginResponse) {
+        Analytics.logLogin(firebaseAnalytics, etRa.text.toString())
+
         PreferencesManager(this).cookie = CookieDTO(cookie.cookie)
         PreferencesManager(this).user = Student(null, null, etRa.text.toString(), null, etPassword.text.toString())
-        Log.d(TAG, "User logged in")
 
         pbLogin.visibility = View.INVISIBLE
         startActivity(Intent(this@LoginActivity, MenuActivity::class.java))
@@ -69,6 +74,7 @@ class LoginActivity : AppCompatActivity(), LoginPresenter.View {
     }
 
     override fun badLogin(errorMessage: String) {
+        Analytics.logBadEvent(firebaseAnalytics, etRa.text.toString(), "badLogin - $errorMessage")
         Log.d(TAG, "Couldn't log user in")
         pbLogin.visibility = View.INVISIBLE
         Toast.makeText(this, "Login invalido", Toast.LENGTH_SHORT).show()
